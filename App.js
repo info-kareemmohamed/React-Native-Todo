8392992b0db37539854dc6faa4e2a1bb1a20815a
Kareem mohamed <info.kareemmohamed@gmail.com>
2024-02-09 07:04:00 +0200

    Home Screen

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 // import { StatusBar } from 'expo-status-bar';
 // import { StyleSheet, Text, View } from 'react-native';
 import AddTodoScreen from './Src/Screens/AddTodo';
+import HomeScreen from './Src/Screens/Home';
 
 export default function App() {
   return (
@@ -8,7 +9,7 @@ export default function App() {
     //   <Text>Open up App.js to start working on your app!</Text>
     //   <StatusBar style="auto" />
     // </View>
-    <AddTodoScreen/>
+    <HomeScreen/>
   );
 }
 
